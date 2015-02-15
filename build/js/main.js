f4410ee9fafb7a031291bb53db2fa84fbd7de1e1
andyskiles <networking@andyskiles.com>
2015-02-15 14:10:27 -0500

    Add competition toggle.

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,18 @@
 (function() {
 	'use strict';
 
-	console.log('Slurpit, slurpit real good.');
+	$(document).ready(function() {
+		$('.slider').slick({
+			lazyLoad: 'ondemand',
+			dots: true,
+			infinite: true,
+		});
+
+		$('.competitions').find('.player-comp h2').click(function() {
+
+			$(this).next().slideToggle('fast');
+
+			$('.player-compy table').not($(this).next()).slideUp('fast');
+		});
+	});
 })();
